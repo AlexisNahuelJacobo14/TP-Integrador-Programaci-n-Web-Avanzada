10c8e851749bbdb23f22dc7f928e96ae93f9ab9c
AlexisNahuelJacobo14 <alexisnahuel.jacobo@alumnos.uai.edu.ar>
2025-05-09 20:29:02 -0300

    Trabajé sobre el formulario de contacto, agregué la vista parcial _Confirmacion para mostrar los datos del Cliente cargado y realicé modificaciones en el Index

--- Integrador_Web_Avanz/Controllers/ClienteController.cs
+++ Integrador_Web_Avanz/Controllers/ClienteController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Integrador_Web_Avanz.Models;
 
 namespace Integrador_Web_Avanz.Controllers
 {
     public class ClienteController : Controller
     {
-        public IActionResult Index()
+        
+
+        [HttpGet]
+        public IActionResult Contacto()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Crear(Cliente cliente)
+        {
+            TempData["Mensaje"] = $"Se cargaron satisfactoriamente los datos: {cliente.Nombre} | {cliente.Apellido} | {cliente.Mail} | {cliente.Telefono}";
+            return View("~/Views/Home/Contacto.cshtml", cliente);
+        }
+
     }
 }
