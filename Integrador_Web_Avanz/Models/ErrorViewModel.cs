3eab2fa3a14f555b5130958af86de324d9a0fb5b
AlexisNahuelJacobo14 <alexisnahuel.jacobo@alumnos.uai.edu.ar>
2025-06-15 17:17:35 -0300

    Ignorada la carpeta .vs y preparado commit limpio

--- Integrador_Web_Avanz/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Integrador_Web_Avanz.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
